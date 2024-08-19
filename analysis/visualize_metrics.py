5a37d6d555ba41a133d4d96d73c6b2a28523f366
Jason Gerard <jasongerard321@gmail.com>
2024-08-19 11:29:07 -0400

    Update evaluation scenarios

--- analysis/visualize_metrics.py
+++ analysis/visualize_metrics.py
@@ -9,7 +9,7 @@ plt.rcParams.update({'font.size': 18})
 plt.rc('legend', fontsize=14)
 plt.rcParams.update({'font.family': 'Times New Roman'})
 
-report_id = 1723687797
+report_id = 1724043930
 path = os.path.join("reports", str(report_id), f"{report_id}_report.csv")
 with open(path, "r") as f:
     report = [{k: v for k, v in row.items()} for row in csv.DictReader(f, skipinitialspace=True)]
@@ -22,17 +22,18 @@ for run in report:
 
 # pprint.pprint(report)
 
-display_name = {
-    "lls": "LLS",
-    "fcp": "FCP",
-    "random": "Random",
-}
+algorithms = [
+    ("lls", "LLS"),
+    ("fcp", "FCP"),
+    ("random", "Random"),
+    ("alternating", "Alternating"),
+]
 
 metrics = [
     ("Capacity", "terabits/day", 20, 200, 20),
     ("Wasted capacity", "terabits/day", 40, 280, 40),
-    ("Scheduled delay", "seconds", 2_000, 10_000, 2_000),
-    ("Jain's fairness index", "", 0.9, 1.0, 0.02),
+    ("Scheduled delay", "seconds", 2_000, 50_000, 10_000),
+    ("Jain's fairness index", "", 0.2, 1.0, 0.2),
 ]
 
 x = [
@@ -47,10 +48,10 @@ for metric, unit, y_min, y_max, y_step in metrics:
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
-    for algorithm in ["fcp", "random", "lls"]:
+    for algorithm, display_name in algorithms:
         y = [run[metric] for run in report if run["Algorithm"] == algorithm]
 
-        plt.plot(x, y, label=display_name[algorithm], linewidth=2.5)
+        plt.plot(x, y, label=display_name, linewidth=2.5)
 
     label = f"{metric} [{unit}]" if unit else metric
     plt.ylabel(label)
@@ -60,7 +61,7 @@ for metric, unit, y_min, y_max, y_step in metrics:
     plt.grid(linestyle='-', color='0.95')
     
     plt.ylim(max(y_min-y_step, 0), y_max)
-    ax.set_yticks(np.arange(y_min, y_max+0.01, y_step))
+    ax.set_yticks([y_min] + np.arange(y_step, y_max+0.01, y_step).tolist())
 
     file_name = label.replace(" ", "_").replace("/", "_")
     plt.savefig(
